71399d5432c9a38af03a27e6d1c717e0fd403288
nldoc <jasa@posteo.de>
2018-08-13 15:37:43 +0200

    final bugfix NetLogo 5.3.1 batch/sh creation

--- R/util_runnl.R
+++ R/util_runnl.R
@@ -291,66 +291,6 @@ util_read_write_batch <- function(nl) {
       ## Make sh executable on linux:
       system(paste0("chmod +x ", batchpath_temp), wait = TRUE)
 
-      # # Block1 of netlogo-headless.sh:
-      # block1 <- c("#!/bin/bash",
-      #             "")
-      #
-      # # Basedirline:
-      # basedirline <- paste0("BASE_DIR=\"", getnl(nl, "nlpath"), "\"")
-      #
-      # # Block2 of netlogo-headless.sh:
-      # block2 <- c("",
-      #             "if [[ ${JAVA_HOME+1} ]]; then",
-      #             "  JAVA=\"${JAVA_HOME}/bin/java\"",
-      #             "else",
-      #             "  echo \"JAVA_HOME undefined, using java from path. For control over exact java version, set JAVA_HOME\"",
-      #             "  JAVA=\"java\"",
-      #             "fi;",
-      #             "")
-      #
-      # # jvmoptsline:
-      # jvmoptsline <- paste0("JVM_OPTS=(-Xmx", getnl(nl, "jvmmem"), "m -XX:+UseParallelGC -Dfile.encoding=UTF-8)")
-      #
-      # # Block3 of netlogo-headless.sh:
-      # block3 <- c("OPTS_INDEX=2",
-      #             "",
-      #             "ARGS=()",
-      #             "INDEX=0",
-      #             "",
-      #             "for arg in \"$@\"; do",
-      #             "  if [[ \"$arg\" == \"--3D\" ]]; then",
-      #             "    JVM_OPTS[OPTS_INDEX++]=\"-Dorg.nlogo.is3d=true\"",
-      #             "  elif [[ \"$arg\" == -D* ]]; then",
-      #             "    JVM_OPTS[OPTS_INDEX++]=\"$arg\"",
-      #             "  else",
-      #             "    ARGS[INDEX++]=\"$arg\"",
-      #             "  fi",
-      #             "done",
-      #             "",
-      #             "RAW_CLASSPATH=\"app/args4j-2.0.12.jar:app/asm-all-5.0.4.jar:app/asm-all-5.0.4.jar:app/autolink-0.6.0.jar:app/behaviorsearch.jar:app/commons-codec-1.10.jar:app/commons-logging-1.1.1.jar:app/config-1.3.1.jar:app/flexmark-0.20.0.jar:app/flexmark-ext-autolink-0.20.0.jar:app/flexmark-ext-escaped-character-0.20.0.jar:app/flexmark-ext-typographic-0.20.0.jar:app/flexmark-formatter-0.20.0.jar:app/flexmark-util-0.20.0.jar:app/gluegen-rt-2.3.2.jar:app/httpclient-4.2.jar:app/httpcore-4.2.jar:app/httpmime-4.2.jar:app/jcommon-1.0.16.jar:app/jfreechart-1.0.13.jar:app/jhotdraw-6.0b1.jar:app/jmf-2.1.1e.jar:app/jogl-all-2.3.2.jar:app/json-simple-1.1.1.jar:app/log4j-1.2.16.jar:app/macro-compat_2.12-1.1.1.jar:app/macro-compat_2.12-1.1.1.jar:app/netlogo-6.0.3.jar:app/parboiled_2.12-2.1.3.jar:app/parboiled_2.12-2.1.3.jar:app/picocontainer-2.13.6.jar:app/picocontainer-2.13.6.jar:app/rsyntaxtextarea-2.6.0.jar:app/scala-library-2.12.0.jar:app/scala-library.jar:app/scala-parser-combinators_2.12-1.0.4.jar:app/scala-parser-combinators_2.12-1.0.5.jar:app/shapeless_2.12-2.3.2.jar:app/shapeless_2.12-2.3.2.jar\"",
-      #             "CLASSPATH=''",
-      #             "",
-      #             "for jar in `echo $RAW_CLASSPATH | sed 's/:/ /g'`; do",
-      #             "  CLASSPATH=\"$CLASSPATH:$BASE_DIR/$jar\"",
-      #             "done",
-      #             "",
-      #             "CLASSPATH=`echo $CLASSPATH | sed 's/://'`",
-      #             "",
-      #             "java \"${JVM_OPTS[@]}\" -Dnetlogo.extensions.dir=\"${BASE_DIR}/app/extensions\" -classpath \"$CLASSPATH\" org.nlogo.headless.Main \"${ARGS[@]}\""
-      # )
-      #
-      # #
-      # # Put all blocks together:
-      # allblocks <- c(block1, basedirline, block2, jvmoptsline, block3)
-      #
-      #
-      # ## Write batch file:
-      # batchpath_temp <- tempfile(pattern="netlogo-headless", fileext=".sh")
-      # writeLines(allblocks, batchpath_temp)
-      #
-      # ## Make sh executable on linux:
-      # system(paste0("chmod +x ", batchpath_temp), wait = TRUE)
-
     } else {
       ## For all other NetLogo versions we can just copy and modify the headless sh from the installation folder:
       ## Create path variables:
