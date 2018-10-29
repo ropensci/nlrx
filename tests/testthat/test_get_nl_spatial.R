c62e761a8c763fe87798c3c9b58e067bac74d58f
nldoc <jasa@posteo.de>
2018-10-29 17:17:16 +0100

    travis_smallfix

--- tests/testthat/test_get_nl_spatial.R
+++ tests/testthat/test_get_nl_spatial.R
@@ -91,7 +91,7 @@ testthat::test_that("Get nl spatial", {
   testthat::expect_match(class(results.spatial)[1], "tbl_df")
 
   testthat::context("Get spatial data without turtles: tibble")
-  results.spatial <- get_nl_spatial(nl, turtles = FALSE,patches=TRUE,
+  results.spatial <- get_nl_spatial(nl, turtles = FALSE, patches=TRUE,
                                     format = "tibble")
   testthat::expect_match(class(results.spatial)[1], "tbl_df")
 
