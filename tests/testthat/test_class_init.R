1c48f541f73c7be313846457c73517e34a80cd83
marco <sciaini.marco@gmail.com>
2018-11-04 15:45:56 +0100

    testing around

--- tests/testthat/test_class_init.R
+++ tests/testthat/test_class_init.R
@@ -1,17 +1,19 @@
 testthat::context("Class initialization")
 testthat::test_that("class objects are initialized", {
 
- .initClasses()
- class(nl)
- nl <- methods::new("nl")
+ # .initClasses()
+ # class(nl)
+ # nl <- methods::new("nl")
  # exp <- methods::new("experiment")
  # sd <- methods::new("simdesign")
- #
+
+ nl <- nl()
+
  testthat::expect_match(class(nl)[[1]], "nl")
  # testthat::expect_match(class(exp)[[1]], "experiment")
  # testthat::expect_match(class(sd)[[1]], "simdesign")
 
- testthat::expect_match(class(nl())[1], "nl")
+ # testthat::expect_match(class(nl())[1], "nl")
  # testthat::expect_match(class(experiment())[1], "experiment")
  # testthat::expect_match(class(simdesign())[1], "simdesign")
 
