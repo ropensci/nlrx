2e20301ada6a9e633ecd9353c2228de21cc8b980
Jenny Bryan <jenny.f.bryan@gmail.com>
2025-11-19 16:37:48 -0800

    Use `readr::write_csv(file =)` instead of `path`

--- R/write_simoutput.R
+++ R/write_simoutput.R
@@ -52,6 +52,5 @@ setsim(nl, \"simoutput\") <- results")
            ".csv"))
 
   # Write output
-  readr::write_csv(x = getsim(nl, "simoutput"), path = outfilename)
+  readr::write_csv(x = getsim(nl, "simoutput"), file = outfilename)
 }
-
